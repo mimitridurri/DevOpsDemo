82003921bbd0e4a50842c8c2bcbc8ad68aedd170
Dimitri <murridim@students.zhaw.ch>
2024-04-07 16:18:42 +0200

    Schritt 2 (also adapted the Assertions import again)

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumerals.java
@@ -0,0 +1,8 @@
+package ch.zhaw.iwi.devops.romanNumerals;
+
+public class RomanNumerals {
+
+    public static String arabicToRoman(int arabic) {
+    return "I";
+    }
+}
--- backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumeralsTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumeralsTest.java
@@ -1,13 +1,13 @@
 package ch.zhaw.iwi.devops.romanNumerals;
 
-import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralsTest {
- 
+
     @Test
     public void one() {
-      Assert.assertEquals("1", "I", RomanNumerals.arabicToRoman(1));
+        Assertions.assertEquals("I", RomanNumerals.arabicToRoman(1));
     }
-   
-  }
+
+}
