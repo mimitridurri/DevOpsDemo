5bea977bacb8866fce004809eb4ff4022885cfed
Dimitri <murridim@students.zhaw.ch>
2024-04-07 15:56:11 +0200

    Schritt 3

--- backend/src/test/java/ch/zhaw/iwi/devops/christmaslight/LightGridTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/christmaslight/LightGridTest.java
@@ -9,4 +9,13 @@ public class LightGridTest {
         LightGrid lightGrid = new LightGrid();
 
     }
+    @Test
+    public void should_trun_on_all_lights() {
+        LightGrid lightGrid = new LightGrid();
+        lightGrid.turnOn(CoordinatePair.of(0, 0, 999, 999));
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                assertThat(lightGrid.getLight(i, j).isOn(), is(true));
+        }
+    }
 }
