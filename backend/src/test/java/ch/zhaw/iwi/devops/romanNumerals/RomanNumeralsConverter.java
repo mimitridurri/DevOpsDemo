9e62818604d86fbf2739099ca62c3a9ca6c6c32b
Dimitri <murridim@students.zhaw.ch>
2024-04-07 16:52:58 +0200

    Schritt 4

--- backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumeralsConverter.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumeralsConverter.java
@@ -3,6 +3,9 @@ package ch.zhaw.iwi.devops.romanNumerals;
 public class RomanNumeralsConverter {
 
     public Object toRoman(int i) {
+        if (i == 2) {
+            return "II";
+        }
         return "I";
     }
 
