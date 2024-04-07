5c5ca6bbc55e112f59d52f900714acb102344e51
Dimitri <murridim@students.zhaw.ch>
2024-04-07 16:53:36 +0200

    Schritt 5

--- backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumerals.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumerals.java
@@ -13,5 +13,6 @@ public class RomanNumerals {
 
         Assertions.assertEquals("I", romanNumerals.toRoman(1));
         Assertions.assertEquals("II", romanNumerals.toRoman(2));
+        Assertions.assertEquals("III", romanNumerals.toRoman(3));
     }
 }
