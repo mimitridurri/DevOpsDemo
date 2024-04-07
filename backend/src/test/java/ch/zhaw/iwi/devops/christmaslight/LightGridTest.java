bd52477bdd8c221a0084778419067444ec6d9b70
Dimitri <murridim@students.zhaw.ch>
2024-04-07 15:51:38 +0200

    Schritt 1

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/christmaslight/LightGridTest.java
@@ -0,0 +1,12 @@
+package ch.zhaw.iwi.devops.christmaslight;
+
+import org.junit.jupiter.api.Test;
+
+public class LightGridTest {
+    @Test
+    public void should_create_light_grid(){
+        
+        LightGrid lightGrid = new LightGrid();
+
+    }
+}
