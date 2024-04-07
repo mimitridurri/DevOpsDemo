680996a7a3b43e247cbe9a5ed0c105de139a38d9
Dimitri <murridim@students.zhaw.ch>
2024-04-07 17:10:58 +0200

    Schritt 14

--- backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumeralsConverter.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/RomanNumeralsConverter.java
@@ -5,6 +5,11 @@ public class RomanNumeralsConverter {
     public Object toRoman(int i) {
         StringBuilder roman = new StringBuilder();
 
+        if (i >= 10) {
+            roman.append("X");
+            i -= 10;
+        }
+
         if (i >= 5) {
             roman.append("V") ;
             i -= 5;
